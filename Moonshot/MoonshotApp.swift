dfbc6ae724c1b8af3fdceb4ba7db0c80d0bedea1
derya <derya@boonandmoil.dev>
2022-07-04 08:32:19 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Derya Antonelli on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
